282a4b39ca26762745f4a4ce05dfe0d717054ea1
楊鈞皓 <john705810420@gmail.com>
2020-07-12 22:13:21 +0800

    宣傳頁：第一次commit。

---
+++ main_page/js/main.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+    
+  $(".btn-menu").click(function(){
+    let $this = $(this);
+    if(! $(".btn-menu").hasClass("open")) {
+      $this.next().css("display","list-item");
+      $this.addClass("open");
+    }
+    else if($(".btn-menu").hasClass("open")) {
+      $this.next().css("display","none");
+      $this.removeClass("open");
+    }
+  });
+
+  $(".has-second-list").click(function(){
+    let $this = $(this);
+    $this.find(".second-floor-in-list").slideToggle();
+  })
+
+  $(window).scroll(function(){
+    var winheight = $(window).scrollTop();
+    if(winheight>0){
+      $(".nav-outer").css("background","#32416B");
+      $(".nav-outer").addClass("scrollNav-animate");
+    }
+    else{
+      $(".nav-outer").css("background","unset");$(".nav-outer").removeClass("scrollNav-animate");
+    }
+  });
+
+  /*警告 這段jquery會吃掉網頁暫存 不建議使用*/
+   $(window).resize(function(){
+     if($(window).width()>1200 && $(".btn-menu").hasClass("open")) {
+      $(".btn-menu").trigger('click');
+      $(".ham").removeClass("active");
+    }
+   });
+  /*警告 上段jquery會吃掉網頁暫存 不建議使用*/
+});
+
+
